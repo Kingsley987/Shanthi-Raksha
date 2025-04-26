2aba5df875342c7712087df47c159d563e46fba9
Kingsley987 <kingsleygowrisankar2@gmail.com>
2025-04-26 22:11:56 +0530

    Create supabaseClient.ts

---
+++ supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://swluzgcfhirqflxxztoq.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bHV6Z2NmaGlycWZseHh6dG9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjAxMDIsImV4cCI6MjA2MTIzNjEwMn0.7KsHNZU1D7zRI-s-_DVTjQV2hSh3WY6LwLkJytKH5aM';          // Your anon key
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
